6a527e723d02bdcffb606d98d7ddea4b8f5eff06
bicaluv <boss.mueller@gmail.com>
2022-09-17 20:00:46 +0200

    Update Motor.cpp

--- Motor.cpp
+++ Motor.cpp
@@ -3,14 +3,17 @@
 
 Motor::Motor() {
   pinMode(MOTOR_PIN, OUTPUT);
-  digitalWrite(MOTOR_PIN, LOW);
-}
-
-void Motor::start() {
   digitalWrite(MOTOR_PIN, HIGH);
 }
 
-void Motor::stop() {
+void Motor::start() {
+  DEBUG_PRINTLN("start");
+  
   digitalWrite(MOTOR_PIN, LOW);
 }
 
+void Motor::stop() {
+  DEBUG_PRINTLN("stop");
+  
+  digitalWrite(MOTOR_PIN, HIGH);
+}
